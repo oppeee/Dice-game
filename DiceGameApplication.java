b904b0d048babb601991a117eb992462c6fe44ed
広口壮太 <oppeee@wlan-napt-001.u-aizu.ac.jp>
2025-06-27 13:40:48 +0900

    Won/Lost message added

--- DiceGameApplication.java
+++ DiceGameApplication.java
@@ -1,20 +1,41 @@
-import java.util.*
+import java.util.*;
 
 class DiceGameApplication{
-    int roll_dice(void){
+    int roll_dice(){
         Random rand = new Random();
         int sum;
         
-        dice1 = rand.nextInt(6) + 1;
-        dice2 = rand.nextInt(6) + 1;
+        int dice1 = rand.nextInt(6) + 1;
+        int dice2 = rand.nextInt(6) + 1;
         sum = dice1 + dice2;
 
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1: " + dice1);
+        System.out.println("Die 2: " + dice2);
         System.out.println("Total value: " + sum);
 
         return sum;
     }
 
-    public static void maiin(String args[]){
-        int total = roll_dice;
+    String Greeting(){
+        System.out.println("What is your name?");
+        System.out.print("> ");
+
+        Scanner scan = new Scanner(System.in);
+        String username = scan.next();
+
+        System.out.println("Hello, " + username);
+        
+        return username;
+    }
+
+    public static void main(String args[]){
+        DiceGameApplication game = new DiceGameApplication();
+
+        String username = game.Greeting();
+        int total = game.roll_dice();
+
+        if(total > 8) System.out.println("You win!");
+        else System.out.println("You lost!");
     }
 }
